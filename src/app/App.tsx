d670c4298414aa93f9f2af507b61012f728345c0
Nariman Mustafaev <abonent26pg@mail.ru>
2025-03-19 15:25:46 +0300

    back entry page upd

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -27,9 +27,7 @@ function App() {
     if(localStorage.getItem('initialPath_encar') === "/" && !isWide){
       navigate('/cars');
     }
-    if(localStorage.getItem('initialPath_encar') === "/" || localStorage.getItem('initialPath_encar') === "/cars" && !isWide){
-      navigate('/cars');
-    }
+
     TG_WEB_APP.disableVerticalSwipes();
     TG_WEB_APP.expand();
   }, []);
